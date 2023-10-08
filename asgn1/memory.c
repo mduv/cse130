26c0deefcda1b4b25aac95d4ee6570b9a4789fbd
Mohan Duvvuri <mduvvuri@ucsc.edu>
2023-10-08 00:31:56 +0000

    finished read and some of get

--- asgn1/memory.c
+++ asgn1/memory.c
@@ -1,5 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
-int main() {
-    return 0;
+#define MAX_INPUTLINE_SIZE 4096
+#define MAX_BUFFER_SIZE 4096
+
+
+/* HANDLE GET */
+void get(const char *location) {
+    // open file @ location for reading
+    int fd = open(location, O_RDONLY);
+    if (fd == -1) {
+        perror("Unable to open file");
+        exit(1);
+    }
+
+    // read and write to stdout
+    char buf[MAX_BUFFER_SIZE];
+    int bytesRead;
+    while ((bytesRead = read(fd, buf, MAX_BUFFER_SIZE)) > 0) {
+        write(1, buf, bytesRead);
+    }
+
+    write(1, buf, bytesRead);
+
+    // close file
+    close(fd);
 }
+
+
+int main(void) {
+    /* READ INPUT COMMAND FROM STDIN */
+    // char* inputLine = (char*)calloc(100, sizeof(char));
+    char inputLine[MAX_INPUTLINE_SIZE];
+    int bytesRead = read(0, inputLine, 4096);
+    if (bytesRead <= 0) {
+        perror("Something went wrong");
+        exit(1);
+    }
+
+
+    /* Parse out the command and the file name ( " get\nfoo.txt\n "), seperate by new line */
+
+    // get the first token (aka the command)
+    char* token = strtok(inputLine, "\n");
+
+    while (token != 0) {
+        // see if it's a get command
+        if (strcmp(token, "get") == 0) {
+            token = strtok(NULL, "\n");
+            if (token != NULL) {
+                get(token);
+            } else {
+                perror("Invalid get command");
+            }
+        }
+        // printf( "%s\n", token);
+        // get next token
+        token = strtok(NULL, "\n");
+    }
+
+    return 0;
+    
+
+}
+
+
+
