e4106767bb8cdb1bbb434eb271ab9d5dd0651388
Mohan Duvvuri <mduvvuri@ucsc.edu>
2023-11-08 18:42:38 +0000

    basic tests working

--- asgn3/queue.c
+++ asgn3/queue.c
@@ -5,12 +5,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-
 // Define the struct queue
 struct queue {
     int size;
     int front;
     int rear;
+    int count;
     void **elements;
     pthread_mutex_t lock;
     pthread_cond_t not_empty;
@@ -18,6 +18,48 @@ struct queue {
 };
 
 // Rest of your implementation...
+
+// In queue.c
+bool queue_push(queue_t *q, void *elem) {
+    pthread_mutex_lock(&q->lock);
+
+    while (q->count == q->size) {
+        // Queue is full, wait for it to become not full
+        pthread_cond_wait(&q->not_full, &q->lock);
+    }
+
+    q->elements[q->rear] = elem;
+    q->rear = (q->rear + 1) % q->size;
+    q->count += 1;
+
+    pthread_cond_signal(&q->not_empty);
+    printf("Signaled not_empty in push\n");  // Add this line
+    pthread_mutex_unlock(&q->lock);
+
+    return true;
+}
+
+// In queue.c
+bool queue_pop(queue_t *q, void **elem) {
+    pthread_mutex_lock(&q->lock);
+
+    while (q->count == 0) {
+        // Queue is empty, wait for it to become not empty
+        pthread_cond_wait(&q->not_empty, &q->lock);
+    }
+
+    *elem = q->elements[q->front];
+    q->front = (q->front + 1) % q->size;
+    q->count -= 1;
+
+    pthread_cond_signal(&q->not_full);
+    printf("Signaled not_full in pop\n");  // Add this line
+    pthread_mutex_unlock(&q->lock);
+
+    return true;
+}
+
+// In queue.c
 queue_t *queue_new(int size) {
     queue_t *q = malloc(sizeof(queue_t));
     if (q == NULL) {
@@ -34,6 +76,7 @@ queue_t *queue_new(int size) {
         return NULL;
     }
 
+    q->count = 0;  // Initialize count to 0
     pthread_mutex_init(&q->lock, NULL);
     pthread_cond_init(&q->not_empty, NULL);
     pthread_cond_init(&q->not_full, NULL);
@@ -51,52 +94,8 @@ void queue_delete(queue_t **q) {
     pthread_mutex_destroy(&(*q)->lock);
     pthread_cond_destroy(&(*q)->not_empty);
     pthread_cond_destroy(&(*q)->not_full);
-    
+
     free(*q);
     *q = NULL;
-}
-
-// In queue.c
-bool queue_push(queue_t *q, void *elem) {
-    pthread_mutex_lock(&q->lock);
-
-    while ((q->rear + 1) % q->size == q->front) {
-        // Queue is full, wait for it to become not full
-        printf("Queue is full. Waiting for it to become not full...\n");  // Add this line
-        pthread_cond_wait(&q->not_full, &q->lock);
-        printf("Woke up from wait. Checking queue again...\n");  // Add this line
-    }
-
-    q->elements[q->rear] = elem;
-    q->rear = (q->rear + 1) % q->size;
-
-    pthread_cond_signal(&q->not_empty);
-    printf("Signaled not_empty in push\n");  // Add this line
-    pthread_mutex_unlock(&q->lock);
-
-    return true;
-}
-
-
-bool queue_pop(queue_t *q, void **elem) {
-    pthread_mutex_lock(&q->lock);
-
-    printf("Entering queue_pop\n");
-
-    while (q->front == q->rear) {
-        // Queue is empty, wait for it to become not empty
-        printf("Queue is empty. Waiting for it to become not empty...\n");
-        pthread_cond_wait(&q->not_empty, &q->lock);
-    }
-
-    *elem = q->elements[q->front];
-    q->front = (q->front + 1) % q->size;
-
-    pthread_cond_signal(&q->not_full);
-    pthread_mutex_unlock(&q->lock);
-
-    printf("Exiting queue_pop\n");
-
-    return true;
 }
 
---
+++ asgn3/rwlock.c
@@ -0,0 +1,98 @@
+// In rwlock.c
+#include "rwlock.h"
+#include <pthread.h>
+#include <stdlib.h>
+
+struct rwlock {
+    pthread_mutex_t lock;
+    pthread_cond_t readers_cv;
+    pthread_cond_t writers_cv;
+    int readers;
+    int writers;
+    PRIORITY priority;
+    int n;
+};
+
+rwlock_t *rwlock_new(PRIORITY p, uint32_t n) {
+    rwlock_t *rw = malloc(sizeof(rwlock_t));
+    if (rw == NULL) {
+        // Handle memory allocation failure
+        return NULL;
+    }
+
+    pthread_mutex_init(&rw->lock, NULL);
+    pthread_cond_init(&rw->readers_cv, NULL);
+    pthread_cond_init(&rw->writers_cv, NULL);
+    rw->readers = 0;
+    rw->writers = 0;
+    rw->priority = p;
+    rw->n = n;
+
+    return rw;
+}
+
+void rwlock_delete(rwlock_t **rw) {
+    if (*rw == NULL) {
+        return;
+    }
+
+    pthread_mutex_destroy(&(*rw)->lock);
+    pthread_cond_destroy(&(*rw)->readers_cv);
+    pthread_cond_destroy(&(*rw)->writers_cv);
+
+    free(*rw);
+    *rw = NULL;
+}
+
+void reader_lock(rwlock_t *rw) {
+    pthread_mutex_lock(&rw->lock);
+
+    while ((rw->priority == READERS && rw->writers > 0) ||
+           (rw->priority == WRITERS && (rw->readers > 0 || rw->writers > 0)) ||
+           (rw->priority == N_WAY && rw->writers > 0 && rw->readers < rw->n)) {
+        pthread_cond_wait(&rw->readers_cv, &rw->lock);
+    }
+
+    rw->readers++;
+
+    pthread_mutex_unlock(&rw->lock);
+}
+
+void reader_unlock(rwlock_t *rw) {
+    pthread_mutex_lock(&rw->lock);
+
+    rw->readers--;
+
+    if (rw->readers == 0 && rw->writers > 0) {
+        pthread_cond_signal(&rw->writers_cv);
+    }
+
+    pthread_mutex_unlock(&rw->lock);
+}
+
+void writer_lock(rwlock_t *rw) {
+    pthread_mutex_lock(&rw->lock);
+
+    while ((rw->priority == WRITERS && (rw->readers > 0 || rw->writers > 0)) ||
+           (rw->priority == N_WAY && (rw->readers > 0 || rw->writers > 0))) {
+        pthread_cond_wait(&rw->writers_cv, &rw->lock);
+    }
+
+    rw->writers++;
+
+    pthread_mutex_unlock(&rw->lock); 
+}
+
+void writer_unlock(rwlock_t *rw) {
+    pthread_mutex_lock(&rw->lock);
+
+    rw->writers--;
+
+    if (rw->priority == N_WAY && rw->readers < rw->n) {
+        pthread_cond_signal(&rw->readers_cv);
+    } else {
+        pthread_cond_broadcast(&rw->writers_cv);
+    }
+
+    pthread_mutex_unlock(&rw->lock);
+}
